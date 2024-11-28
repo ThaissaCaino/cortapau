90c821987d8c6cbf7b2d9e3fb531e1a51d86784c
ThaissaCaino <almeida.c.thaissa@gmail.com>
2024-11-28 20:16:37 -0300

    mensagem de logout e exclusão do mailer

--- nodejs-conf/node_entrypoint.sh
+++ nodejs-conf/node_entrypoint.sh
@@ -5,7 +5,7 @@ cd /home/node/app
 npm -g uninstall nodemon --save
 npm install nodemon
 
-npm install express cors multer bcrypt passport body-parser passport-local-mongoose jsonwebtoken nodemailers ejs express-ejs-layouts  bcryptjs express-session
+npm install express cors multer bcrypt passport body-parser passport-local-mongoose jsonwebtoken ejs express-ejs-layouts  bcryptjs express-session
 
 
 npm run start.dev
